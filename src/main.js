0cdfce3d3d33f75b7eab74b116c56250bb82fd65
Mathijs H <mattie123@live.nl>
2021-01-03 02:32:39 +0100

    Adding packages to app

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from "vue";
 import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+// import VueRouter from "vue-router";/
+import { store } from "./store/store";
 
 Vue.config.productionTip = false;
 
 new Vue({
+  vuetify,
+  // router,
+  store,
   render: h => h(App)
 }).$mount("#app");
+
+// Vue.use(VueRouter);
+
+// const routes = {
+
+// }
